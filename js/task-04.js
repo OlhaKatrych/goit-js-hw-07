d39dfc9aad543e6e532411f735700686faa99d48
OlhaKatrych <olgak198931@gmail.com>
2023-12-18 11:49:56 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+
+  const elems = evt.target.elements;
+
+  const data = {
+    email: elems.email.value.trim(),
+    password: elems.password.value.trim(),
+  };
+  if (data.email === "" && data.password === "") {
+    alert("All form fields must be filled in");
+  }
+  form.reset();
+}
