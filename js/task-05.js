d1a38a7ac82fbef3bdf848b7691d9b4e26150cf6
OlhaKatrych <olgak198931@gmail.com>
2023-12-18 12:35:53 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,14 @@
+const backGrColor = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", handlerClick);
+
+function handlerClick() {
+  const body = document.querySelector("body");
+  const generateColor = (body.style.backgroundColor = getRandomHexColor());
+  backGrColor.textContent = generateColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
